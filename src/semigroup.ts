2a96cecd850d8f806b6fe2d34220285b1a030960
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-07-11 09:39:20 +0100

    feat(semigroup): define semigroups for some primitives

--- src/semigroup.ts
+++ src/semigroup.ts
@@ -1,16 +1,76 @@
+import assert from "node:assert";
 import { Semigroup } from "fp-ts/Semigroup";
 
-function concat<T>(semigroup: Semigroup<T>, x: T, y: T): T {
-  return semigroup.concat(x, y);
+{
+  function concat<T>(semigroup: Semigroup<T>, x: T, y: T): T {
+    return semigroup.concat(x, y);
+  }
+
+  const SemigroupString: Semigroup<string> = {
+    concat: (x, y) => `${x}${y}`,
+  };
+
+  const SemigroupNumberAdd: Semigroup<number> = {
+    concat: (x, y) => x + y,
+  };
+
+  const SemigroupNumberMul: Semigroup<number> = {
+    concat: (x, y) => x * y,
+  };
+
+  const SemigroupBooleanOr: Semigroup<boolean> = {
+    concat: (x, y) => x || y,
+  };
+
+  const SemigroupBooleanAnd: Semigroup<boolean> = {
+    concat: (x, y) => x && y,
+  };
+
+  {
+    const actual = concat(SemigroupString, "domain", "expansion");
+    const expect = "domainexpansion";
+    assert.equal(actual, expect);
+  }
+
+  {
+    const actual = concat(SemigroupNumberAdd, 10, 5);
+    const expect = 15;
+    assert.equal(actual, expect);
+  }
+
+  {
+    const actual = concat(SemigroupNumberMul, 10, 5);
+    const expect = 50;
+    assert.equal(actual, expect);
+  }
+
+  {
+    const actual = concat(SemigroupBooleanAnd, true, false);
+    const expect = false;
+    assert.equal(actual, expect);
+  }
+
+  {
+    const actual = concat(SemigroupBooleanAnd, true, true);
+    const expect = true;
+    assert.equal(actual, expect);
+  }
+
+  {
+    const actual = concat(SemigroupBooleanOr, true, false);
+    const expect = true;
+    assert.equal(actual, expect);
+  }
+
+  {
+    const actual = concat(SemigroupBooleanOr, true, true);
+    const expect = true;
+    assert.equal(actual, expect);
+  }
+
+  {
+    const actual = concat(SemigroupBooleanOr, true, true);
+    const expect = true;
+    assert.equal(actual, expect);
+  }
 }
-
-const SemigroupString: Semigroup<string> = {
-  concat: (x, y) => `${x}${y}`,
-};
-
-const SemigroupNumberAdd: Semigroup<number> = {
-  concat: (x, y) => x + y,
-};
-
-console.log(concat(SemigroupString, "hello", "world"));
-console.log(concat(SemigroupNumberAdd, 10, 20));
