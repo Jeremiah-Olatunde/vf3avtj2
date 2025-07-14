2573900711e0c050c609567a22f43b164a7f81bf
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-07-14 02:35:35 +0100

    feat(task): Task.fromIO

--- src/task.ts
+++ src/task.ts
@@ -1,5 +1,7 @@
 import assert from "node:assert";
 
+import * as IO from "fp-ts/IO";
+
 import * as TCore from "fp-ts/Task";
 import * as TStd from "fp-ts-std/Task";
 import { type Task } from "fp-ts/Task";
@@ -10,4 +12,12 @@ const T = { ...TCore, ...TStd };
   const task: Task<string> = T.of("hello task");
   const actual = await T.execute(task);
   assert.deepStrictEqual(actual, "hello task");
+})();
+
+(async function () {
+  const random = Math.random(); // for testing purpose
+  const getRandom: IO.IO<number> = IO.of(random);
+  const task = T.fromIO(getRandom);
+  const actual = await T.execute(task);
+  assert.deepStrictEqual(actual, random);
 })();
